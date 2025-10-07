65f832edd88858364a80d31dee03c6e033f3b015
islamalama22 <islamalama22@gmail.com>
2025-10-08 02:05:59 +0300

    file reader

--- createUser.js
+++ createUser.js
@@ -1,6 +1,25 @@
+
 //  bothe  of them  return same  data  
 //const createUserForm=document.querySelector(".createUserForm");
 const createUserForm = document.querySelector(".createUserForm");
+const previewImage = document.querySelector("#prieviwe");
+
+createUserForm.image.addEventListener('change', () => {
+    //  it  will  return  a  obj  of  files
+    const file = createUserForm.image.files[0];
+
+    ///  read  the file  
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    //  when  the  read is  load 
+
+    reader.addEventListener("load", (e) => {
+        previewImage.setAttribute("src", e.target.result);
+      });
+
+
+});
 
 
 createUserForm.addEventListener("submit", async (e) => {
@@ -8,20 +27,28 @@ createUserForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
 
-    //  name  email age  must be  same  name  as  backend  json and  the  input  name  must  be  same  aslo 
-    const user = {
-        Name: createUserForm.name.value,
-        Email: createUserForm.email.value,
-        Age: createUserForm.age.value
-    }
-    
+    //  the  data is  with  file  so  must  be  formdata
+    //  it  will  take all  the  inputs by  the  attrebut of  name  in html  ,  so  its  better  to  give  it name  same  as  backend
+    const formData = new FormData(createUserForm);
 
-    console.log(user);
-    // api  post  to  send  the  data  thet inputs  by  user  in  form 
-    //  post  fun  and  send the obj 
-    const response = await axios.post("http://ums12.runasp.net/api/users", user);
-    console.log(response);
-})
+    //  to  see  the  data in  formdata  must  do  loop  in it 
+    //  there  is  :  value  which is  the input  of  user  
+    //  there  is :   key which  is  the name  ,  email,pass
+    formData.forEach((value, key) => {
+        console.log(`key : ${key}, value : ${value}`);
+    })
+
+
+    const respones = await axios.post("http://ums12.runasp.net/api/users", formData);
+
+    if (respones.status == 200) {
+
+        location.href = 'index.html';
+        console.log("  the  creat  is  done  and  go  to  index.html");
+    }
+    console.log(respones);
+
+});
 
 
 
@@ -40,25 +67,25 @@ createUserForm.addEventListener("submit", async (e) => {
 
 const JS_HELP = {
     console: {
-        log(){}, warn(){}, error(){}
+        log() { }, warn() { }, error() { }
     },
     window: {
-        alert(){}, prompt(){}, confirm(){}, setTimeout(){}, setInterval(){}, clearTimeout(){}, clearInterval(){}
+        alert() { }, prompt() { }, confirm() { }, setTimeout() { }, setInterval() { }, clearTimeout() { }, clearInterval() { }
     },
     document: {
-        getElementById(){}, getElementsByClassName(){}, getElementsByTagName(){}, querySelector(){}, querySelectorAll(){},
-        createElement(){}, createTextNode(){}, appendChild(){}, removeChild(){}, replaceChild(){}, addEventListener(){}, removeEventListener(){},
-        getAttribute(){}, setAttribute(){}, classList: { add(){}, remove(){}, toggle(){}, contains(){} }, innerHTML(){}, textContent(){}
+        getElementById() { }, getElementsByClassName() { }, getElementsByTagName() { }, querySelector() { }, querySelectorAll() { },
+        createElement() { }, createTextNode() { }, appendChild() { }, removeChild() { }, replaceChild() { }, addEventListener() { }, removeEventListener() { },
+        getAttribute() { }, setAttribute() { }, classList: { add() { }, remove() { }, toggle() { }, contains() { } }, innerHTML() { }, textContent() { }
     },
     Array: {
-        isArray(){}, from(){}, prototype: {
-            map(){}, forEach(){}, filter(){}, reduce(){}, find(){}, findIndex(){}, includes(){}, push(){}, pop(){}, shift(){}, unshift(){}, sort(){}, reverse(){}
+        isArray() { }, from() { }, prototype: {
+            map() { }, forEach() { }, filter() { }, reduce() { }, find() { }, findIndex() { }, includes() { }, push() { }, pop() { }, shift() { }, unshift() { }, sort() { }, reverse() { }
         }
     },
     String: {
-        prototype: { split(){}, join(){}, trim(){}, includes(){}, startsWith(){}, endsWith(){}, indexOf(){}, replace(){}, toUpperCase(){}, toLowerCase(){} }
+        prototype: { split() { }, join() { }, trim() { }, includes() { }, startsWith() { }, endsWith() { }, indexOf() { }, replace() { }, toUpperCase() { }, toLowerCase() { } }
     },
-    Math: { random(){}, floor(){}, ceil(){}, round(){}, max(){}, min(){} },
-    Date: { now(){}, prototype: { getDate(){}, getMonth(){}, getFullYear(){} } },
-    Promise: { then(){}, catch(){}, finally(){} },
+    Math: { random() { }, floor() { }, ceil() { }, round() { }, max() { }, min() { } },
+    Date: { now() { }, prototype: { getDate() { }, getMonth() { }, getFullYear() { } } },
+    Promise: { then() { }, catch() { }, finally() { } },
 };
--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const getUsers = async () => {
+  const getUsers = async () => {
     //  the  api  is  for  get  user  
     const response = await axios.get("http://ums12.runasp.net/api/users");
     //  the  strucure  of  data  get  it  from  the  respones 
+    console.log("response.data   :");
     console.log(response.data);
     //  to  be  public  make  it  let  and out  sid the  if 
     let result = "";
     if (response.status == 200) {
         // 
         result = response.data.users
+
         //  
             .map((user) => {
                 return `
@@ -17,11 +19,11 @@ const getUsers = async () => {
                  <td> ${user.name}</td>
                  <td> ${user.age}</td>
                  <td> ${user.email}</td>
+                 <td> <img src="${user.imageUrl}" width="200px" />   </td>
+
                 <td> 
                   <a href='./detasil.html?user_id=${user.id}' class="btn btn-outline-dark"> detasils</a>
                    <button class="btn btn-outline-danger " onclick=deleteUser("${user.id}") > delete</button>
-
-
                 </td>
 
             </tr> `;
